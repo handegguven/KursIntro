af3555e821224ae93306ec13beba03d36871eb55
huawei <huawei@LAPTOP-E578LRHQ>
2021-02-07 21:58:31 +0300

    4.Hafta Ödevi 5. Ödev

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> AdYas = new Dictionary<string, int>();
+            AdYas.Add("Ecrin Şevval", 12);
+            AdYas.Add("Kübra Kelci",10);
+            AdYas.Add("Hande Güven",20);
+
+            foreach(var degeroku in AdYas)
+            {
+                Console.WriteLine(degeroku);
+            }
+
+
+            
+            {
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
